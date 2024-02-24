ab65e5828a1f36f6fdc7a6b86079f7df714c58fc
jdlopez12 <josephdlopez7@gmail.com>
2024-02-24 16:44:03 -0500

    Complete assignment 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    let reversedWord = '';
+    for (let index = word.length - 1; index >= 0; index--) {
+        reversedWord = reversedWord + word[index];
+    }
+    return reversedWord;
 };
 
 // Do not edit below this line
