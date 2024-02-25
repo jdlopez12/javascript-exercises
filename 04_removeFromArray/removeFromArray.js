90b62726d841a5cdd16433c5421e43ebb6e1907a
jdlopez12 <josephdlopez7@gmail.com>
2024-02-25 16:49:54 -0500

    Complete #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
 
+    let removedArr = arr.filter(item => !toRemove.includes(item));
+    return removedArr;
 };
 
 // Do not edit below this line
